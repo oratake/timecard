da03f4ae85423f6fa410644c84e750b4935cac44
oratake <smith.shimomura@gmail.com>
2025-07-24 16:56:46 +0900

    feat: axumを0.8に更新し、不要なimportを削除
    - axumのバージョンを0.7から0.8に更新 - `src/main.rs` から不要な `use std::net::SocketAddr;` を削除

--- timecard-api/src/main.rs
+++ timecard-api/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use tokio;
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    info!("starting server");
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
 }
